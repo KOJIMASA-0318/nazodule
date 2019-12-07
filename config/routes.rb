5f9c2d65f755fa2213890111e4e8762a6df5d56d
KOJIMASA-0318 <masayuki.k.2020@gmail.com>
2019-12-08 00:04:00 +0900

    usernew

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
+
+  resources :users
 end
