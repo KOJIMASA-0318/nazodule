832d86f80d169a8452ffc2a2d0bce45baf418ec7
KOJIMASA-0318 <masayuki.k.2020@gmail.com>
2019-12-16 19:30:50 +0900

    pre device

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email_params)
+    if user && user.authenticate(password_params[:password])
+      log_in user
+      redirect_to root_path, success: 'ログインに成功しました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  private
+   def log_in(user)
+     session[:user_id] = user.id
+   end
+
+   def log_out
+     session.delete(:user_id)
+     @current_user = nil
+   end
+
+   def email_params
+     params.require(:session).permit(:email)
+   end
+
+   def password_params
+     params.require(:session).permit(:password)
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  get 'events/new'
+  get 'sessions/new'
   get 'users/new'
   get 'pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+
   resources :users
+  resources :mainpages
+
+
 end
